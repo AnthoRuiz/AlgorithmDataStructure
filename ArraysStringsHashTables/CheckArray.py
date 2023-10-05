dec44cbd88590dbf14a4bee1efaaff7802981e55
Anthony Ruiz <yobakaj@gmail.com>
2023-10-05 14:55:12 -0400

    optimized to only check ASCII characters

--- ArraysStringsHashTables/CheckArray.py
+++ ArraysStringsHashTables/CheckArray.py
@@ -1,4 +1,4 @@
-# Brute Force
+# Brute Force O(N^2)
 def is_unique_brute(s):
     for i in range(len(s)):
         for j in range(i + 1, len(s)):
@@ -7,16 +7,23 @@ def is_unique_brute(s):
     return True
 
 
-# Optimizing using HashMap
+# Optimizing using HashMap O(N) or O(1) because we can have more than
+# 128 Characters, we are going to assume that we are working with ASCII
 def is_unique_hash(s):
     hash_s = {}
-    for i in range(len(s)):
-        if s[i].lower() in hash_s.keys():
-            return False
-        else:
-            hash_s[s[i].lower()] = s[i]
+    MAX_ASCII = 128
+
+    if len(s) > MAX_ASCII:
+        return False
+    else:
+        for i in range(len(s)):
+            if s[i].lower() in hash_s.keys():
+                return False
+            else:
+                hash_s[s[i].lower()] = s[i]
+
     return True
 
 
 if __name__ == '__main__':
-    print(is_unique_hash("bbbB"))
+    print(is_unique_hash("ABC"))
