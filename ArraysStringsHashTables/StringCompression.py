7da7e9b63a555223125fb0898fdd01354cef5a92
Anthony Ruiz <yobakaj@gmail.com>
2023-10-23 06:26:28 -0400

    string compression and updated readme

---
+++ ArraysStringsHashTables/StringCompression.py
@@ -0,0 +1,39 @@
+def string_compression(s):
+    result = ''
+    counter = 1
+    i = 0
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
+            counter += 1
+        else:
+            result += s[i] + str(counter)
+            counter = 1
+    result += s[i] + str(counter)
+    return result
+
+
+def compress(chars):
+    nu_chars = len(chars)
+
+    if nu_chars < 2:
+        return nu_chars
+
+    i = j = 0
+    while i < nu_chars:
+        count = 1
+        while i < len(chars) - 1 and chars[i] == chars[i + 1]:
+            count += 1
+            i += 1
+        chars[j] = chars[i]
+        j += 1
+        if count > 1:
+            for val in str(count):
+                chars[j] = val
+                j += 1
+        i += 1
+
+    return j
+
+
+if __name__ == '__main__':
+    print(compress(["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"]))
--- README.md
+++ README.md
@@ -20,6 +20,7 @@
 - Climbing Stairs **(Leetcode #70)** [here](https://github.com/AnthoRuiz/AlgorithmDataStructure/blob/main/ArraysStringsHashTables/ClimbingStairs.py)
 - Find the Index of the First Occurrence in a String **(Leetcode #28)** [here](https://github.com/AnthoRuiz/AlgorithmDataStructure/blob/main/ArraysStringsHashTables/NeedleHaystack.py)
 - Kth Distinct String in an Array **(Leetcode #2053)** [here](https://github.com/AnthoRuiz/AlgorithmDataStructure/blob/main/ArraysStringsHashTables/KthDistinct.py)
+- String Compression **(Leetcode #443)** [here](https://github.com/AnthoRuiz/AlgorithmDataStructure/blob/main/ArraysStringsHashTables/StringCompression.py)
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
